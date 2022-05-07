594c55ce2f26879dc1bb82a743ba6f09756a716d
LucasChoi <vv@gmail.com>
2022-05-08 05:02:27 +0800

    no message

--- network.js
+++ network.js
@@ -1,5 +1,6 @@
 /*
-Used to set the network: https://chainlist.org/
+Used to set the network
+
 1 Mainnet
 3 Ropsten
 4 Rinkeby
@@ -13,13 +14,8 @@ Used to set the network: https://chainlist.org/
 80001 Matic Testnet
 43113 Avalanche Testnet
 43114 Avalanche Mainnet
-42220 Celo Mainnet
-44787 Celo Alfajores Testnet
-62320 Celo Baklava Testnet
-1666700000 Harmony Testnet Shard0
-1666600000 Harmony Mainnet Shard0
-25 Cronos Mainnet Beta
-338 Cronos Testnet
+49777 Leprichain Mainnet
 */
 
-window.web3ChainId = 97;
+window.web3NetworkId = 49777;
+window.infuraKey = "2d0062a43e9e4086829df115488b45a8"
